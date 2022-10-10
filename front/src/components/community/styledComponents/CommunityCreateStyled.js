dafe1bbcf455dc6e8f08c8039adffe5de0ee6e96
euion <qq212420@gmail.com>
2022-10-10 13:36:23 +0900

    등록(연두),모집(파랑)버튼추가

--- front/src/components/community/styledComponents/CommunityCreateStyled.js
+++ front/src/components/community/styledComponents/CommunityCreateStyled.js
@@ -3,12 +3,8 @@ import styled from "styled-components";
 const TitleAlign = styled.div`
   display: flex;
   justify-content: space-between;
-  .community-title-button {
-    background-color: #9ac355;
-    border: #9ac355 solid;
-    border-radius: 8px;
-  }
 `;
+
 const AllContentAlign = styled.div`
   padding: 20px 10%;
 `;
