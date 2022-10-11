17bdaabaaeb84b560123d4bc3cdbd583432c125f
[AI5/1반] 현지예 <hyeonjiyeah@gmail.com>
2022-10-11 16:01:48 +0900

    헤더 구현

--- front/src/components/common/navigation/Navigation.js
+++ front/src/components/common/navigation/Navigation.js
@@ -1,53 +1,104 @@
-import React, { useContext } from "react";
+import React, { useRef } from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
 import { Layout, Menu } from "antd";
-import { useNavigate, useLocation } from "react-router-dom";
+import { theme } from "../styles/palette";
 // import { UserStateContext, DispatchContext } from "../App";
 
 function Navigation() {
-  const { Header, Footer, Sider, Content } = Layout;
-  //   const navigate = useNavigate();
-  //   const location = useLocation();
+  const { Header } = Layout;
 
   //   const userState = useContext(UserStateContext);
   //   const dispatch = useContext(DispatchContext);
 
   //   const isLogin = !!userState.user;
+  const homeMenu = useRef(null);
+
+  const HeaderLight = styled(Header)`
+    background-color: white;
+    border-bottom: 1px solid #f0f0f0;
+  `;
+
   const isLogin = false;
 
-  const navs = [
-    { label: "홈", key: "home" },
-    { label: "산찾기", key: "search" },
-    { label: "커뮤니티", key: "community" },
-    { label: "로그인", key: "login" },
-  ];
+  const NavMenu = styled(Menu)`
+    float: right;
+    font-size: 16px;
+
+    & > .ant-menu-item-selected a,
+    & > .ant-menu-item a:hover {
+      color: ${theme.naturalGreen};
+      font-weight: 700;
+    }
+
+    & .ant-menu-item::after,
+    .ant-menu-submenu::after {
+      border: none !important;
+    }
+  `;
+
+  const NavLink = styled(Link)`
+    box-sizing: border-box;
+    display: inline-block;
+    color: #000;
+    &:hover {
+      color: #9ac355;
+    }
+  `;
+
+  const Logo = styled.div`
+    float: left;
+    width: 180px;
+    height: 44px;
+    cursor: pointer;
+  `;
+
+  const LogoImage = styled.img`
+    height: 100%;
+    width: 100%;
+    object-fit: contain;
+    padding: 4px 10px 8px 0;
+  `;
+
+  function handleClick() {
+    console.log(homeMenu.current);
+    homeMenu.current.click();
+  }
 
   return (
-    <div>header</div>
-    // <Layout>
-    //   <Header>
-    //     <div className="logo">LOGO</div>
-    //     <Menu
-    //       mode="horizontal"
-    //       defaultSelectedKeys={["1"]}
-    //       items={navs.map((nav) => {
-    //         return { key: nav.key, label: nav.label };
-    //       })}
-    //     />
-    //     {isLogin ? (
-    //       <div
-    //       //   onClick={() => navigate("/users/:userId")}
-    //       >
-    //         MYPAGE ICON
-    //       </div>
-    //     ) : (
-    //       <div
-    //       //    onClick={() => navigate("/login")}
-    //       >
-    //         로그인
-    //       </div>
-    //     )}
-    //   </Header>
-    // </Layout>
+    <Layout>
+      <HeaderLight>
+        <Link to="/">
+          <Logo onClick={handleClick}>
+            <LogoImage src="/Logo.png" />
+          </Logo>
+        </Link>
+        <NavMenu mode="horizontal" defaultSelectedKeys={["1"]}>
+          <Menu.Item key="1">
+            <Link to="/" ref={homeMenu}>
+              <span>홈</span>
+            </Link>
+          </Menu.Item>
+          <Menu.Item key="2">
+            <Link to="/detail">
+              <span>산찾기</span>
+            </Link>
+          </Menu.Item>
+          <Menu.Item key="3">
+            <Link to="/community">
+              <span>커뮤니티</span>
+            </Link>
+          </Menu.Item>
+          <Menu.Item key="4">
+            {isLogin ? (
+              <Link to="/users/:userId">MYPAGE ICON</Link>
+            ) : (
+              <Link to="/login">로그인</Link>
+            )}
+          </Menu.Item>
+        </NavMenu>
+      </HeaderLight>
+    </Layout>
   );
 }
 
--- front/src/components/common/styles/palette.js
+++ front/src/components/common/styles/palette.js
@@ -1,25 +1,8 @@
-import { css } from "styled-components";
-
 export const theme = {
   primary: "#9AC355",
-  naturalGreen: "89A550",
-  blue: "#3A6AD4",
+  naturalGreen: "#89A550",
+  mainBlue: "#3A6AD4",
   deepBlue: "#304659",
   lightGrey: "#F2F2F2",
   black: "#000",
-
-  fontLogo: "'Song Myung', serif",
-  fontMain: "'Gugi'",
-
-  fontMain: "80px",
-  fontLarge: "64px",
-  fontMedium: "27px",
-  fontSemiMedium: "24px",
-  fontRegular: "21px",
-  fontSmall: "16px",
-  fontMicro: "14px",
-
-  weightBold: 700,
-  weightSemiBold: 600,
-  weightRegular: 400,
 };
