afb7005bef4f8b76c16f524f88cfa74228e310d3
euion <qq212420@gmail.com>
2022-10-11 14:29:10 +0900

    nav바,pagenation토대

--- front/src/components/community/component/CommunityCreate.js
+++ front/src/components/community/component/CommunityCreate.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { DatePicker, Input, Select } from "antd";
 import { EnvironmentOutlined } from "@ant-design/icons";
 import {
@@ -8,8 +8,16 @@ import {
 } from "../styledComponents/CommunityCreateStyled";
 import TextArea from "antd/lib/input/TextArea";
 import { RegisterGreenBtn } from "../../common/button/IconBtn";
+import { Form } from "react-router-dom";
+
 const { Option } = Select;
 function CommunityCreate() {
+  const [componentSize, setComponentSize] = useState("default");
+
+  const onFormLayoutChange = ({ size }) => {
+    setComponentSize(size);
+  };
+
   const onChange = (e) => {
     console.log("Change:", e.target.value);
   };
@@ -20,25 +28,22 @@ function CommunityCreate() {
           <h1>글 작성</h1>
         </TitleAlign>
         <RegisterGreenBtn />
-        <form>
+        <Form>
           <Input placeholder="Basic usage" />
           <CommunityFormSecond>
+            <DatePicker />
+            <Form.Item label="Select">
+              <Select>
+                <Select.Option value="demo">Demo</Select.Option>
+              </Select>
+            </Form.Item>
             <Select defaultValue="Option1">
               <Option value="Option1">말머리 선택</Option>
               <Option value="Option2">Option2</Option>
             </Select>
-            <Input
-              placeholder="default size"
-              prefix={<EnvironmentOutlined />}
-            />
-            <DatePicker
-              style={{
-                width: "70%",
-              }}
-            />
           </CommunityFormSecond>
           <TextArea showCount maxLength={500} onChange={onChange} />
-        </form>
+        </Form>
       </AllContentAlign>
     </>
   );
