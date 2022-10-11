5118c4b5f8df9cbc96c9526096a8fdcb10252851
[AI5/1반] 현지예 <hyeonjiyeah@gmail.com>
2022-10-11 16:02:56 +0900

    메인 배너 디자인 수정

--- front/src/components/main/MainBanner.js
+++ front/src/components/main/MainBanner.js
@@ -5,15 +5,16 @@ import MainSelection from "./MainSelection";
 function MainBanner() {
   const Box = styled.div`
     max-width: 100%;
-    height: 800px;
-    background: url("./backgroundImg.png") no-repeat bottom;
+    height: 860px;
+    padding-top: 196px;
+    background: url("/backgroundImage.png") no-repeat bottom;
     background-size: contain;
     background-color: #e2efff;
     background-blend-mode: normal;
+    background-position: bottom;
     text-align: center;
     display: flex;
     justify-content: center;
-    align-items: center;
   `;
 
   const ContentWrapper = styled.div``;
@@ -24,6 +25,14 @@ function MainBanner() {
     font-weight: 400;
     line-height: 100px;
     letter-spacing: -0.6px;
+    margin-bottom: 0px;
+  `;
+
+  const Description = styled.div`
+    margin: 10px 0 30px 0;
+    font-size: 16px;
+
+    animation: fadeInUp 1s;
   `;
 
   return (
@@ -32,7 +41,7 @@ function MainBanner() {
         <Title>
           다같이 줍자<br></br>#클린하이킹
         </Title>
-        <div>클린 하이커스는 ~한 서비스입니다.</div>
+        <Description>클린 하이커스는 ~한 서비스입니다.</Description>
         <MainSelection />
       </ContentWrapper>
     </Box>
