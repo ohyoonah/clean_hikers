175d636377142e46d85e1a22b0f512f0624e09da
[AI5/1반] 현지예 <hyeonjiyeah@gmail.com>
2022-10-13 17:27:25 +0900

    path값 변경, 커뮤니티에 선택값 넘겨주기 구현

--- front/src/components/main/MainSelection.js
+++ front/src/components/main/MainSelection.js
@@ -1,12 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
-import { Select, DatePicker, Space } from "antd";
-import { NonIconBlueBtn } from "../common/button/NonIconBtn";
+import { Form, Select, DatePicker } from "antd";
+import { NonIconBlueBtnStyled } from "../common/button/NonIconBtnStyled";
 import { theme } from "../common/styles/palette";
+import { ROUTES } from "../../enum/routes";
+
 const { Option } = Select;
 
 function MainSelection() {
-  const Container = styled.div`
+  const [form] = Form.useForm();
+
+  const navigate = useNavigate();
+
+  const regions = [
+    "서울",
+    "경기",
+    "인천",
+    "강원",
+    "충북",
+    "충남",
+    "경북",
+    "경남",
+    "전북",
+    "전남",
+    "부산",
+    "제주",
+  ];
+
+  const counts = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+  const FormWrapper = styled(Form)`
     /* Display & Box Model */
     display: grid;
     justify-content: center;
@@ -14,11 +38,15 @@ function MainSelection() {
     grid-column-gap: 10px;
     width: 630px;
     height: 72px;
-    padding: 15px;
+    padding: 16px;
     border-radius: 24px;
     margin: 0 auto;
     box-shadow: 0px 9.77379px 11.4028px rgba(67, 83, 138, 0.06);
     align-items: center;
+
+    & button {
+      padding: 0 32px;
+    }
 
     /* Color */
     background-color: white;
@@ -56,58 +84,39 @@ function MainSelection() {
     }
   `;
 
-  const regions = [
-    "서울",
-    "경기",
-    "인천",
-    "강원",
-    "충북",
-    "충남",
-    "경북",
-    "경남",
-    "전북",
-    "전남",
-    "부산",
-    "제주",
-  ];
+  function onFinish(values) {
+    if (values["date"]) values["date"] = values["date"].format("YYYY-MM-DD");
 
-  function handleChange(value) {
-    console.log(`selected ${value}`);
-  }
+    console.log(values);
 
-  function onChange(date, dateString) {
-    console.log(date, dateString);
+    navigate(ROUTES.COMMUNITY.COMMUNITY_CREATE, { state: values });
   }
 
   return (
-    <Container>
-      <SelectWrapper
-        bordered={false}
-        defaultValue="지역"
-        onChange={handleChange}
-      >
-        {regions.map((region, idx) => {
-          return (
-            <Option key={idx} value={region}>
-              {region}
-            </Option>
-          );
-        })}
-      </SelectWrapper>
-      <Space direction="vertical">
-        <DatePickerWrapper
-          bordered={false}
-          placeholder="날짜"
-          onChange={onChange}
-        />
-      </Space>
-      <SelectWrapper
-        bordered={false}
-        defaultValue="인원"
-        onChange={handleChange}
-      ></SelectWrapper>
-      <NonIconBlueBtn text="시작하기" />
-    </Container>
+    <FormWrapper form={form} onFinish={onFinish}>
+      <Form.Item name="region">
+        <SelectWrapper bordered={false} defaultValue="지역">
+          {regions.map((region) => {
+            return <Option key={region}>{region}</Option>;
+          })}
+        </SelectWrapper>
+      </Form.Item>
+      <Form.Item name="date">
+        <DatePickerWrapper bordered={false} placeholder="날짜" />
+      </Form.Item>
+      <Form.Item name="count">
+        <SelectWrapper bordered={false} defaultValue="인원">
+          {counts.map((cnt) => {
+            return <Option key={cnt}>{cnt}명</Option>;
+          })}
+        </SelectWrapper>
+      </Form.Item>
+      <Form.Item>
+        <NonIconBlueBtnStyled type="priamry" size="large" htmlType="submit">
+          시작하기
+        </NonIconBlueBtnStyled>
+      </Form.Item>
+    </FormWrapper>
   );
 }
 
