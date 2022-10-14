3c56e61c07a8b84535f2322379ab1558cb4eaad0
[AI5/1반] 현지예 <hyeonjiyeah@gmail.com>
2022-10-14 20:09:44 +0900

    메인페이지 스타일드 컴포넌트 분리

---
+++ front/src/components/main/MainBannerStyled.js
@@ -0,0 +1,87 @@
+import styled from "styled-components";
+import { Form, Select, DatePicker } from "antd";
+import { theme } from "../common/styles/palette";
+
+// Main Banner Style
+export const Box = styled.div`
+  max-width: 100%;
+  height: 860px;
+  padding-top: 196px;
+  background: url("/backgroundImage.png") no-repeat bottom;
+  background-size: contain;
+  background-color: #e2efff;
+  background-blend-mode: normal;
+  background-position: bottom;
+  text-align: center;
+  display: flex;
+  justify-content: center;
+`;
+
+export const Title = styled.h1`
+  font-family: "Gugi";
+  font-size: 5rem;
+  font-weight: 400;
+  line-height: 100px;
+  letter-spacing: -0.6px;
+  margin-bottom: 0px;
+`;
+
+export const Description = styled.div`
+  margin: 10px 0 30px 0;
+  font-size: 16px;
+  animation: fadeInUp 1s;
+`;
+
+// Selection Box Style
+export const FormWrapper = styled(Form)`
+  /* Display & Box Model */
+  display: grid;
+  justify-content: center;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  grid-column-gap: 10px;
+  width: 630px;
+  height: 72px;
+  padding: 16px;
+  border-radius: 24px;
+  margin: 0 auto;
+  box-shadow: 0px 9.77379px 11.4028px rgba(67, 83, 138, 0.06);
+  align-items: center;
+
+  & button {
+    padding: 0 32px;
+  }
+
+  /* Color */
+  background-color: white;
+`;
+
+export const SelectWrapper = styled(Select)`
+  /* Display & Box Model */
+  text-align: start;
+
+  /* Text */
+  & .ant-select-selection-item,
+  .ant-select-selection-placeholder {
+    font-weight: 600;
+    font-size: 16px;
+    color: ${theme.deepBlue};
+  }
+`;
+
+export const DatePickerWrapper = styled(DatePicker)`
+  /* Display & Box Model */
+  background-color: transparent;
+  text-align: start;
+
+  /* Text */
+  & input {
+    font-weight: 600;
+    font-size: 16px;
+  }
+
+  /* Color */
+  & input,
+  input::placeholder {
+    color: ${theme.deepBlue};
+  }
+`;
--- front/src/components/main/MainSelection.js
+++ front/src/components/main/MainSelection.js
@@ -1,16 +1,18 @@
-import React, { useState } from "react";
+import React from "react";
 import { useNavigate } from "react-router-dom";
-import styled from "styled-components";
-import { Form, Select, DatePicker } from "antd";
+import { Form } from "antd";
+import {
+  FormWrapper,
+  SelectWrapper,
+  DatePickerWrapper,
+} from "./MainBannerStyled";
 import { NonIconBlueBtnStyled } from "../common/button/NonIconBtnStyled";
-import { theme } from "../common/styles/palette";
 import { ROUTES } from "../../enum/routes";
 
-const { Option } = Select;
+const { Option } = SelectWrapper;
 
 function MainSelection() {
   const [form] = Form.useForm();
-
   const navigate = useNavigate();
 
   const regions = [
@@ -30,72 +32,17 @@ function MainSelection() {
 
   const counts = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-  const FormWrapper = styled(Form)`
-    /* Display & Box Model */
-    display: grid;
-    justify-content: center;
-    grid-template-columns: 1fr 1fr 1fr 1fr;
-    grid-column-gap: 10px;
-    width: 630px;
-    height: 72px;
-    padding: 16px;
-    border-radius: 24px;
-    margin: 0 auto;
-    box-shadow: 0px 9.77379px 11.4028px rgba(67, 83, 138, 0.06);
-    align-items: center;
-
-    & button {
-      padding: 0 32px;
-    }
-
-    /* Color */
-    background-color: white;
-  `;
-
-  const SelectWrapper = styled(Select)`
-    /* Display & Box Model */
-    text-align: start;
-
-    /* Text */
-    & .ant-select-selection-item {
-      font-weight: 600;
-      font-size: 16px;
-    }
-
-    /* Color */
-    color: ${theme.deepBlue};
-  `;
-
-  const DatePickerWrapper = styled(DatePicker)`
-    /* Display & Box Model */
-    background-color: transparent;
-    text-align: start;
-
-    /* Text */
-    & input {
-      font-weight: 600;
-      font-size: 16px;
-    }
-
-    /* Color */
-    & input,
-    input::placeholder {
-      color: ${theme.deepBlue};
-    }
-  `;
-
   function onFinish(values) {
-    if (values["date"]) values["date"] = values["date"].format("YYYY-MM-DD");
-
-    console.log(values);
-
+    if (values["date"]) {
+      values["date"] = values["date"].format("YYYY-MM-DD");
+    }
     navigate(ROUTES.COMMUNITY.COMMUNITY_CREATE, { state: values });
   }
 
   return (
     <FormWrapper form={form} onFinish={onFinish}>
       <Form.Item name="region">
-        <SelectWrapper bordered={false} defaultValue="지역">
+        <SelectWrapper bordered={false} placeholder="지역">
           {regions.map((region) => {
             return <Option key={region}>{region}</Option>;
           })}
@@ -105,7 +52,7 @@ function MainSelection() {
         <DatePickerWrapper bordered={false} placeholder="날짜" />
       </Form.Item>
       <Form.Item name="count">
-        <SelectWrapper bordered={false} defaultValue="인원">
+        <SelectWrapper bordered={false} placeholder="인원">
           {counts.map((cnt) => {
             return <Option key={cnt}>{cnt}명</Option>;
           })}
