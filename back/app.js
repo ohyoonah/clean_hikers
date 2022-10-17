41b53a480b05e4ffe6d42276b1885f364e92af3c
euion <qq212420@gmail.com>
2022-10-17 11:35:06 +0900

    병합수정

--- back/app.js
+++ back/app.js
@@ -15,17 +15,32 @@ app.use(express.urlencoded({ extended: false }));
 const port = 5000;
 
 app.get("/", (req, res) => {
-    res.send("hello world");
+  res.send("hello world");
 });
 
 //라우터 추가
+import { userRouter } from "./src/user/userRouter.js";
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+/*
+@swagger
+/product :
+    get:
+        tags:
+        -pro
+*/
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+
 app.use("/user", userRouter);
-app.use("/community", postRouter);
-app.use("/community", commentRouter);
-app.use("/community", personRouter);
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
 
 app.use(errorMiddleware);
 
 app.listen(port, () => {
-    console.log(`${port}에 연결되었습니다`);
+  console.log(`${port}에 연결되었습니다`);
 });
