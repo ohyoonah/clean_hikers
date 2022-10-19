1e5753e2461450a729c176974a12f4c043b3a091
ohyoonah <dhdbsdk3@naver.com>
2022-10-19 17:37:52 +0900

    유저 포스트 컴포넌트 분리

---
+++ front/src/components/user/UserPostItem.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import {
+  CommunityItemStyled,
+  StateButton,
+} from "../community/styledComponents/CommunityItemstyled";
+
+import { Card, Col, Row } from "antd";
+import { EnvironmentOutlined } from "@ant-design/icons";
+
+import moment from "moment";
+
+function UserPostItem({ post, user }) {
+  const postTime = moment(post.createdAt).fromNow();
+
+  return (
+    <CommunityItemStyled>
+      <Link to={`/community/communityDetail/${post.post_id}`}>
+        <Card hoverable>
+          <Row>
+            <Col span={18} push={0}>
+              <Row>
+                <StateButton>{post.station}</StateButton>
+                <h3> {post.title}</h3>
+              </Row>
+              <p>
+                지역 : {<EnvironmentOutlined />}
+                {post.location.name} | {post.location.address}
+              </p>
+              <p>{post.description}</p>
+            </Col>
+            <Col span={2} push={4}>
+              <p>{postTime}</p>
+            </Col>
+            <p className="writer-user-name">{user.nickname}</p>
+          </Row>
+        </Card>
+      </Link>
+    </CommunityItemStyled>
+  );
+}
+
+export default UserPostItem;
--- front/src/components/user/UserPostList.js
+++ front/src/components/user/UserPostList.js
@@ -1,21 +1,11 @@
 import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
-import moment from "moment";
 
 import * as api from "../../api/api";
+import UserPostIem from "./UserPostItem";
 import BottomNavigation from "../common/navigation/BottomNavigation";
-
-import {
-  CommunityItemStyled,
-  StateButton,
-} from "../community/styledComponents/CommunityItemstyled";
-
-import { Card, Col, Row } from "antd";
-import { EnvironmentOutlined } from "@ant-design/icons";
 
 function UserPostList({ user }) {
   const [userPost, setUserPost] = useState([]);
-  const postTime = moment(userPost.createdAt).fromNow();
 
   useEffect(() => {
     const id = user.id;
@@ -34,29 +24,7 @@ function UserPostList({ user }) {
     <>
       {userPost &&
         userPost.map((post, i) => (
-          <CommunityItemStyled key={userPost[i].post_id}>
-            <Link to={`/community/communityDetail/${post.post_id}`}>
-              <Card hoverable>
-                <Row>
-                  <Col span={18} push={0}>
-                    <Row>
-                      <StateButton>{post.station}</StateButton>
-                      <h3> {post.title}</h3>
-                    </Row>
-                    <p>
-                      지역 : {<EnvironmentOutlined />}
-                      {post.location.name} | {post.location.address}
-                    </p>
-                    <p>{post.description}</p>
-                  </Col>
-                  <Col span={2} push={4}>
-                    <p>{postTime}</p>
-                  </Col>
-                  <p className="writer-user-name">{user.nickname}</p>
-                </Row>
-              </Card>
-            </Link>
-          </CommunityItemStyled>
+          <UserPostIem key={userPost[i].post_id} post={post} user={user} />
         ))}
       <BottomNavigation props={userPost.length} />
     </>
