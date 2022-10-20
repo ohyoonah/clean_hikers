21c30d7343a2ec8ec5b3ca1cd3d104df6810c90a
[AI5/1반] 현지예 <hyeonjiyeah@gmail.com>
2022-10-20 18:07:42 +0900

    스타일드 오류 수정, 버튼추가

--- front/src/components/main/PromoBanner.js
+++ front/src/components/main/PromoBanner.js
@@ -1,9 +1,18 @@
-import React, { useEffect } from "react";
-import { BannerWrapper } from "./PromoBannerStyled";
+import React, { useContext, useEffect } from "react";
+import { UserStateContext } from "../../App";
+import { useNavigate } from "react-router-dom";
+import { BannerWrapper, NonIconBlueBtn } from "./PromoBannerStyled";
 import AOS from "aos";
 import "aos/dist/aos.css";
+import { Row, Col } from "antd";
+import { ROUTES } from "../../enum/routes";
 
 function PromoBanner() {
+  const navigate = useNavigate();
+  const userState = useContext(UserStateContext);
+
+  const isLogin = !!userState.user;
+
   useEffect(() => {
     AOS.init({
       duration: 1000,
@@ -11,11 +20,28 @@ function PromoBanner() {
     });
   });
 
+  function handleClick() {
+    if (isLogin) navigate(ROUTES.COMMUNITY.ROOT);
+    else navigate(ROUTES.USER.LOGIN);
+  }
+
   return (
     <BannerWrapper>
-      <span data-aos="fade-up">
-        지구를 살리는 클린하이킹, <br></br>여러 사람들과 함께해보세요.
-      </span>
+      <Col span={24} data-aos="fade-up">
+        <span>
+          지구를 살리는 클린하이킹, <br></br>여러 사람들과 함께해보세요.
+        </span>
+      </Col>
+      <Col span={24} data-aos="fade-up">
+        <NonIconBlueBtn
+          type="priamry"
+          size="large"
+          htmlType="submit"
+          onClick={handleClick}
+        >
+          시작하기
+        </NonIconBlueBtn>
+      </Col>
     </BannerWrapper>
   );
 }
